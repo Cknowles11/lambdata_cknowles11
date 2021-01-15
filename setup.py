6b98439e328eb50a506b4a6926929222bb790a83
CJ Knowles <cknowles4211@gmail.com>
2021-01-14 21:10:32 -0600

    created setup.py file to push to test.pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-    setup(
-        name="helper_functions"
-    )
+setup(
+    name="help_func", # the name that you will install via pip
+    version="1.0",
+    author="Carlos Knowles",
+    author_email="cknowles4211@gmail.com",
+    description="A package containing two helper functions to add a series to a dataframe and to convert string date time data to date time format",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/Cknowles11/lambdata_cknowles11",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
